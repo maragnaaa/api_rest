8199f04afaed40341e9341d683554bea30c3bfff
Gustavo <gustavomaragna88@gmail.com>
2026-08-04 16:55:54 -0300

    created a complete CRUD for the products db

--- src/index.js
+++ src/index.js
@@ -68,6 +68,49 @@ app.get('/products', async (req, res) => {
     }
 });
 
+app.post('/products', async (req, res) => {
+    try {
+        const newProduct = await prisma.products.create({
+            data: req.body
+        });
+        res.json(newProduct)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(error)
+    }
+});
+
+app.put('/products/:code', async (req, res) => {
+    try {
+        const updateProduct = await prisma.products.update({
+            where: {
+                code: Number(req.params.code)
+            },
+            data: req.body
+        });
+        res.json(updateProduct)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(error)
+    }
+});
+
+app.delete('/products/:code', async (req, res) => {
+    try {
+        const deleteProduct = await prisma.products.delete({
+            where: {
+                code: Number(req.params.code)
+            }
+        });
+    
+        const users = await prisma.products.findMany()
+        res.json(users)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(error)
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`server connected to port ${PORT}`);
 });
