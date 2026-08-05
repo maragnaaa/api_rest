db28cf79dd298b78e139af685a50c91deb2f7dc2
Gustavo <gustavomaragna88@gmail.com>
2026-08-05 16:25:29 -0300

    added products model

---
+++ src/models/productsModel.js
@@ -0,0 +1,55 @@
+import { prisma } from "../config/prisma";
+
+async function findAllproducts() {
+    return prisma.products.findMany();
+}
+
+async function findProductByCode(code) {
+    return prisma.products.findUnique({
+        where: {
+            code: Number(code)
+        }
+    });
+}
+
+async function findProductByName(name) {
+    return prisma.products.findUnique({
+        where: {
+            name
+        }
+    });
+}
+
+async function create(data) {
+    return prisma.products.create({
+        data
+    });
+}
+
+async function update(code, data) {
+    return prisma.products.update({
+        where: {
+            code: Number(code)
+        },
+        data
+    });
+}
+
+async function remove(code) {
+    return prisma.products.delete({
+        where: {
+            code: Number(code)
+        }
+    });
+}
+
+const productsModel = {
+    findAllproducts,
+    findProductByCode,
+    findProductByName,
+    create,
+    update,
+    remove
+}
+
+export { productsModel };
