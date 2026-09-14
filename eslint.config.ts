1d788f9505a0586c930443a2f7e925fb89a3d47d
Gustavo <gustavomaragna88@gmail.com>
2026-09-14 19:38:01 -0300

    added error handler

--- eslint.config.ts
+++ eslint.config.ts
@@ -10,6 +10,15 @@ export default defineConfig([
     plugins: { js },
     extends: ['js/recommended'],
     languageOptions: { globals: globals.node },
+
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
   },
   tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
---
+++ src/shared/middlewares/errorHandler.ts
@@ -0,0 +1,16 @@
+import type { Request, Response, NextFunction } from 'express';
+import { AppError } from '../errors/appError.ts';
+import { ZodError } from 'zod';
+
+export function errorHandler(_req: Request, res: Response, err: Error, _next: NextFunction) {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ error: err.message });
+  }
+
+  if (err instanceof ZodError) {
+    return res.status(400).json({ error: err.flatten });
+  }
+
+  console.log(err);
+  return res.status(500).json({ error: 'Internal server error' });
+}
