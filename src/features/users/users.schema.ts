de215038f80c87d098edd34de46183bb6d642f8d
Gustavo <gustavomaragna88@gmail.com>
2026-09-15 00:25:38 -0300

    added users schemas

---
+++ src/features/users/users.schema.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod';
+
+export const createUserSchema = z.object({
+  name: z
+    .string()
+    .nonempty()
+    .regex(/^[a-z]+$/),
+  admin: z.boolean().default(false).nonoptional(),
+  id_code: z.coerce.number().positive().nonoptional(),
+});
+
+export const updateUserSchema = createUserSchema.partial();
+
+export const idUserParamSchema = z.object({
+  id_code: z.coerce.number().positive().nonoptional(),
+});
+
+export const nameUserParamSchema = z.object({
+  name: z
+    .string()
+    .nonempty()
+    .regex(/^[a-z]+$/),
+});
