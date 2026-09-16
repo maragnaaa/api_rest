73f8fd45859006cb47dd9bedb08274103440e502
Gustavo <gustavomaragna88@gmail.com>
2026-09-15 22:49:58 -0300

    changed endpoints

--- src/features/users/users.routes.ts
+++ src/features/users/users.routes.ts
@@ -11,27 +11,31 @@ import {
 
 const usersRoutes = Router();
 
-usersRoutes.post('/', validate(createUserSchema, 'body'), asyncHandler(usersController.register));
-usersRoutes.get('/', usersController.listAll);
+usersRoutes.post(
+  '/users',
+  validate(createUserSchema, 'body'),
+  asyncHandler(usersController.register),
+);
+usersRoutes.get('/users', usersController.listAll);
 usersRoutes.get(
-  '/name/:name',
+  '/users/:name',
   validate(nameUserParamSchema, 'params'),
   asyncHandler(usersController.showByName),
 );
 usersRoutes.get(
-  '/id/:id_code',
+  '/users/:id_code',
   validate(idUserParamSchema, 'params'),
   asyncHandler(usersController.showById),
 );
 usersRoutes.put(
-  '/:id_code',
+  '/users/:id_code',
   validate(updateUserSchema, 'body'),
   validate(idUserParamSchema, 'params'),
   asyncHandler(usersController.update),
   asyncHandler(usersController.showById),
 );
 usersRoutes.delete(
-  '/:id_code',
+  '/users/:id_code',
   validate(idUserParamSchema, 'params'),
   asyncHandler(usersController.showById),
   asyncHandler(usersController.destroy),
